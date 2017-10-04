3d3d4a2b1f330691a43d8ed0d8caf2bddcae8b36
Jonss <joaosantana.ti@gmail.com>
2017-10-03 23:10:30 -0300

    aula 2.1

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-var app = require('express')();
-app.set('view engine', 'ejs');
+var app = require('./config/express')();
 
 app.get("/produtos", function(req, resp) {
-	//response.send("<h1>Lista de produtos</h1>");
 	resp.render("produtos/lista");
 });
-
 
 app.listen(3000, function() {
 	console.log("servidor rodando!");
