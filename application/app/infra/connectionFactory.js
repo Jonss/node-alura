fd6e523828beac5774f1e5aa118b9e5c27e0e2df
Jonss <joaosantana.ti@gmail.com>
2017-10-04 21:50:06 -0300

    aula 2.16

--- application/app/infra/connectionFactory.js
+++ application/app/infra/connectionFactory.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 
-module.exports = function() {
+function createDbConnection() {
 	return mysql.createConnection({
 		host: process.env.ENV_DB_HOST,
 		user: process.env.ENV_DB_USER,
 		password: process.env.ENV_DB_PASSWORD,
 		database: process.env.ENV_DB_DATABASE
 	});
+}
+
+module.exports = function() {
+	return createDbConnection;
 }
--- application/app/routes/produtos.js
+++ application/app/routes/produtos.js
@@ -1,8 +1,8 @@
-var connectionFactory = require('../infra/connectionFactory');
-
 module.exports = function(app) {
 	app.get("/produtos", function(req, resp) {
-		var connection = connectionFactory();
+
+		var connection = app.infra.connectionFactory();
+
 		connection.query('select * from produtos', function(err, result) {
 			if(err){
 				console.log(err)
