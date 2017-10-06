5980ecc86c84c7132e6c790809379d7fc5ed5e78
Jonss <joaosantana.ti@gmail.com>
2017-10-05 21:46:49 -0300

    aula 2.19

--- application/app/routes/produtos.js
+++ application/app/routes/produtos.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
 	app.get("/produtos", function(req, resp) {
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = app.infra.produtosBanco;
+		var produtosBanco = app.infra.produtosBanco(connection);
 
-		produtosBanco.lista(connection, function(erros,resultados){
+		produtosBanco.lista(function(erros,resultados){
             resp.render('produtos/lista',{lista:resultados});
        	});
 
