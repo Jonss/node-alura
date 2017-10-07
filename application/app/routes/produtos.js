502baeafee0070864923b5fb4f59cb7cbf529813
Jonss <joaosantana.ti@gmail.com>
2017-10-06 22:39:21 -0300

    aula 3.7

--- application/app/routes/produtos.js
+++ application/app/routes/produtos.js
@@ -1,14 +1,35 @@
 module.exports = function(app) {
 	app.get("/produtos", function(req, resp) {
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-		produtosBanco.lista(function(erros,resultados){
+		produtosDAO.lista(function(erros,resultados){
             resp.render('produtos/lista',{lista:resultados});
        	});
 
 		connection.end();
-	});	
+	});
+
+	app.get("/produtos/form", function(req, res) {
+		res.render('produtos/form')
+	});
+
+	app.post('/produtos/salva', function(req, res) {
+		
+		var produto = req.body;
+
+		console.log(produto);
+
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.salva(produto,function(err, result){
+			res.redirect('/produtos');
+		});
+
+		connection.end();
+	});
+
 }
 
 
